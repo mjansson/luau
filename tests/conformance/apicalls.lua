e9e5647640a979a1097da2713841f1f7c76af717
Mattias Jansson <mjansson@gmail.com>
2021-11-04 09:11:15 +0100

    cleanup of repo

--- tests/conformance/apicalls.lua
+++
@@ -1,8 +0,0 @@
--- This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
-print('testing function calls through API')
-
-function add(a, b)
-	return a + b
-end
-
-return('OK')
